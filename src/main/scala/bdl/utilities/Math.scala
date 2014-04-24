2244d98e3f6631d1029c9ee33e8b1d71999f0756
XianXing Zhang <xianxingzhang@gmail.com>
2014-04-24 16:08:31 -0400

    lda intial commit (with bugs including decreasing elbo)

--- src/main/scala/bdl/utilities/Math.scala
+++
@@ -1,9 +0,0 @@
-package utilities
-
-object Math {
-  def sigmoid(value : Double) : Double = {
-    if (value < -10) 4.5398e-05
-    else if (value > 10) 1-1e-05f
-    else 1 / (1 + math.exp(-value))
-  }
-}
